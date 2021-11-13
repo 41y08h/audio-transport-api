6d829a8d925d2f78f22bac79898f0898b4180ab1
Piyush <piyush41y08h@gmail.com>
2021-11-13 19:54:26 +0530

    fix response body in validate username route

--- src/controllers/Auth/validateUsername.ts
+++ src/controllers/Auth/validateUsername.ts
@@ -6,13 +6,24 @@ interface QueryInput {
   username: string;
 }
 
-const validateUsername: RequestHandler = async (req, res) => {
-  const { username } = req.ctx.getQuery<QueryInput>({
-    username: AuthSchema.username,
-  });
+interface ResponseJson {
+  valid: boolean;
+}
 
-  if (await isUsernameAvailable(username)) res.json({ valid: true });
-  else res.status(400).json({ valid: false });
+const validateUsername: RequestHandler<any, ResponseJson> = async (
+  req,
+  res
+) => {
+  try {
+    const { username } = req.ctx.getQuery<QueryInput>({
+      username: AuthSchema.username,
+    });
+
+    if (await isUsernameAvailable(username)) res.json({ valid: true });
+    else res.status(400).json({ valid: false });
+  } catch {
+    res.json({ valid: false });
+  }
 };
 
 export default validateUsername;
