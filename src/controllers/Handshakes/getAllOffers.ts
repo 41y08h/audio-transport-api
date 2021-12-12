aa6ff4eecf421fc43ff1f7d1d306f8a9f4e0d094
Piyush <piyush41y08h@gmail.com>
2021-12-12 17:20:59 +0530

    fix: getAllOffers controller

--- src/controllers/Handshakes/getAllOffers.ts
+++ src/controllers/Handshakes/getAllOffers.ts
@@ -1,9 +1,10 @@
 import { RequestHandler } from "express";
 import Handshake from "../../db/models/Handshake";
 
-const getAllOffers: RequestHandler = async (req, res, next) => {
+const getAllOffers: RequestHandler = async (req, res) => {
   const handshakes = await Handshake.query()
     .where({ fromUserId: req.user.id })
+    .orWhere({ toUserId: req.user.id })
     .withGraphFetched("fromUser(defaultSelects)")
     .withGraphFetched("toUser(defaultSelects)");
 
