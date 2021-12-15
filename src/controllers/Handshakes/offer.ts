5f6f472454ec00ab0a126adf7f6ad6857006282a
Piyush <piyush41y08h@gmail.com>
2021-12-15 07:47:07 +0530

    fix offer controller

--- src/controllers/Handshakes/offer.ts
+++ src/controllers/Handshakes/offer.ts
@@ -2,6 +2,7 @@ import { RequestHandler } from "express";
 import Joi from "joi";
 import { UniqueViolationError } from "objection";
 import Handshake from "../../db/models/Handshake";
+import Peer from "../../db/models/Peer";
 import User from "../../db/models/User";
 
 interface BodyInput {
@@ -28,6 +29,14 @@ const offer: RequestHandler = async (req, res) => {
     });
     if (existingHandshake)
       return req.ctx.error("You already have an offer from this user", 409);
+
+    // Check if already paired
+    const isAlreadyPaired = await Peer.query()
+      .select()
+      .where({ userId: offerToUser.id })
+      .orWhere({ peerId: user.id });
+    if (isAlreadyPaired)
+      return req.ctx.error("You are already paired with this user", 409);
 
     const handshake = await Handshake.query()
       .insertAndFetch({ fromUserId: user.id, toUserId: offerToUser.id })
